e7fc24492453bf3a41e26c63852747f707552186
AlokGDAAS <ankit25792kumar@gmail.com>
2025-11-11 23:54:26 +0530

    rd

--- my-app/src/components/Brand_name.jsx
+++ my-app/src/components/Brand_name.jsx
@@ -4,7 +4,7 @@ export default function Brand_name() {
   return (
     <div className="rf-container ">
       <span className="rf-word rf-first">Rahul</span>
-      <span className="rf-word rf-first">Flowers </span>
+      <span className="rf-word rf-first">Flowers</span>
       
     </div>
   );
